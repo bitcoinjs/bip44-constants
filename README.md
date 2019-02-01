229e24e5326942dffd8ab4efe8a3b190ac8ba894
Jonathan Underwood <junderwood@bitcoinbank.co.jp>
2019-02-02 06:58:18 +0900

    Convert library into 2d Array (#31)

--- README.md
+++ README.md
@@ -17,15 +17,36 @@ const constants = require('bip44-constants')
 console.log(constants)
 
 // iterate through constants
-Object.keys(constants).forEach(coinSymbol => {
-  const constant = constants[coinSymbol]
+// format: constant, coinSymbol, coinName
+// ie. [ 0x80000002, "LTC", "Litecoin" ]
+// Some rows have no coin and are just indices and constants
+constants.forEach(row => {
+  const constant = row[0]
+  const coinSymbol = row[1]
+  const coinName = row[2]
 
   // ...
-  console.log(coinSymbol, constant)
+  console.log(coinName, coinSymbol, constant)
 })
 
-console.log(constants['LTC'])
-// => 2147483650   // equivalent to 0x80000002
+// Find your coin by using filter
+const liteCoin = constants.filter(item => item[1] === 'LTC')
+// ---- OR filter on multiple elements if you are aware of duplicates
+const liquidBitcoin = constants.filter(item => item[1] === 'LBTC' && item[2] === 'Liquid BTC')
+
+// Be sure to assert there was only one result
+assert(liteCoin.length === 1)
+assert(liquidBitcoin.length === 1)
+
+console.log(liteCoin[0])
+// [ 2147483650, 'LTC', 'Litecoin' ]
+console.log(liquidBitcoin[0])
+// [ 2147485424, 'LBTC', 'Liquid BTC' ]
+
+console.log(liteCoin[0][0])
+// 2147483650 (which is 0x80000002 in hex)
+console.log(liquidBitcoin[0][0])
+// 2147485424 (which is 0x800006f0 in hex)
 ```
 
 
--- update.js
+++ update.js
@@ -5,33 +5,47 @@ const fetch = require('node-fetch')
   const res = await fetch('https://github.com/satoshilabs/slips/blob/master/slip-0044.md')
   const body = await res.text()
 
-  const constants = {}
+  const constants = []
   const $ = cheerio.load(body)
   $('table tr').each((i, el) => {
-    const cols = $(el).find('td')
-    const index = $(cols[0]).text()
-    if (!index) return // table header
+    if (i === 0) return // skip the header
 
-    const coin = $(cols[2]).text().trim()
-    if (!coin) return // not defined yet
+    const cols = Array.from($(el).find('td').map((idx, elem) => {
+      return $(elem).text().trim()
+    }))
+
+    // remove the index from the item, it is redundant
+    cols.shift()
+
+    if (cols[1] === '' && cols[2] === '') return // remove empty rows
 
     // turn invisible characters into '?' for visibility when logging
-    const oldConstant = $(cols[1]).text().trim().replace(/[^a-fA-F0-9x]/g, '?')
+    cols[0] = cols[0].replace(/[^a-fA-F0-9x]/g, '?')
     // remove any characters besides 'x' and hex characters
-    const newConstant = oldConstant.normalize('NFKD').replace(/[^a-fA-F0-9x]/g, '')
+    const newConstant = cols[0].normalize('NFKD').replace(/[^a-fA-F0-9x]/g, '')
     if (newConstant.length !== 10 || !newConstant.match(/^0x[a-fA-F0-9]{8}$/)) {
-      console.error(`"${coin}" is improper format: "${oldConstant}" and we couldn't fix it... Skipping.`)
-      return // constant was improper format
+      console.error(`Row #${i} is improper format: "${cols[0]}" and we couldn't fix it... Consider warning in new release`)
+      constants.push(cols)
+      return
     }
-    constants[coin] = newConstant
+    cols[0] = newConstant
+    constants.push(cols)
   })
 
-  console.log('module.exports = {')
-  const keys = Object.keys(constants)
-  keys.sort().forEach((key, i) => {
-    console.log(`  "${key}": ${constants[key]}${i + 1 === keys.length ? '' : ','}`)
+  console.log('// Format for each row:')
+  console.log('// [ constant, coinSymbol, coinName ]')
+  console.log('module.exports = [')
+  constants.forEach((row, i) => {
+    let replaceString = '[$1'
+    if (!row[0].match(/^0x[a-fA-F0-9]{8}$/)) replaceString = '["$1"'
+    let rowString = JSON.stringify(row)
+      .replace(/^\["([^"]*)"/, replaceString)
+      .replace(/,/g, ', ')
+      .replace(/^\[/, '[ ')
+      .replace(/\]$/, ' ]')
+    console.log(`  ${rowString}${i + 1 === constants.length ? '' : ','}`)
   })
-  console.log('}')
+  console.log(']')
 })().catch((err) => {
   console.error(err)
   process.exit(1)

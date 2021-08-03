67484fcfcf056e3a1a5e39e227dd154cd02b4b9f
Slava Fomin II <slava@fomin.io>
2021-08-03 03:10:03 +0300

    Introduced TypeScript declarations
    Signed-off-by: Slava Fomin II <slava@fomin.io>

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+
+declare module 'bip44-constants' {
+
+  export type CoinTypeNumber = number;
+  export type CoinTypeName = string;
+  export type CoinTypeTitle = string;
+
+  export type Entry = [
+    CoinTypeNumber,
+    CoinTypeName,
+    CoinTypeTitle,
+  ];
+
+  const entries: Entry[];
+
+  export default entries;
+
+}
